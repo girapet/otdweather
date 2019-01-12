1ef58f0c1a962bf4cd595e7a9138a780edc75b3e
Peter Girard <peter.h.girard@gmail.com>
2019-01-12 11:11:34 -0500

    switched to untiled, full-screen weather images

--- js/index.js
+++ js/index.js
@@ -1,49 +1,48 @@
 
 const nowCoastUrl = 'https://nowcoast.noaa.gov/arcgis/rest/services/nowcoast/radar_meteo_imagery_nexrad_time/MapServer';
-const nowCoastParams = 'layers=3&bbox={minx}%2C{miny}%2C{maxx}%2C{maxy}&size=256%2C256&f=image&format=png&transparent=true';
+const nowCoastParams = 'layers=3&bbox={minx}%2C{miny}%2C{maxx}%2C{maxy}&size={sx}%2C{sy}&f=image&format=png&transparent=true';
 const getWeatherTemplate = (t) => `${nowCoastUrl}/export?time=${t}&${nowCoastParams}`;
 const weatherOptions = {
   attribution: '© <a href="https://www.weather.gov">NWS</a>',
-  opacity: 0.5,
-  fadeDuration: 400
+  opacity: 0.5
 };
 
 const weather = {
   layers: [],
-  quantity: 5,
+  maxLayers: 7,
+  intervalMilliseconds: 600000,
   time: new Date().valueOf()
 };
 
 const weatherCycle = () => {
-  if (weather.layers[weather.current].isLoading()) {
-    setTimeout(weatherCycle, 400);
-    return;
+  const now = new Date().valueOf();
+
+  if (weather.time < now - weather.intervalMilliseconds) {
+    const layer = L.shingleLayer(getWeatherTemplate(now), { ...weatherOptions, visibility: 'hidden' });
+    weather.layers.push(layer.addTo(map));
+    weather.layers.shift().removeFrom(map);
+    weather.current = (weather.current + weather.maxLayers - 1) % weather.maxLayers;
+    weather.time = now;
   }
 
   const old = weather.current;
-  weather.current = (weather.current + 1) % weather.quantity;
-  weather.layers[weather.current].addTo(map);
-  setTimeout(() => weather.layers[old].removeFrom(map), 1);
-  setTimeout(weatherCycle, weather.current === weather.quantity - 1 ? 3000 : 400);
+  weather.current = (weather.current + 1) % weather.maxLayers;
+  weather.layers[weather.current].show();
+  weather.layers[old].hide();
+  setTimeout(weatherCycle, weather.current === weather.maxLayers - 1 ? 3000 : 400);
 };
 
 const initWeatherLayers = () => {
   if (weather.layers.length > 0) {
     weather.layers[0].off('load', initWeatherLayers);
-
-    if (weather.layers.length > 1) {
-      weather.layers[0].removeFrom(map);
-      weather.layers[0].options.opacity = 0.5;
-    }
   }
 
-  if (weather.layers.length < weather.quantity) {
-    const template = getWeatherTemplate(weather.time - weather.layers.length * 600000);
-    const opacity = weather.layers.length === 0 ? weatherOptions.opacity : 0.0001;
-    const layer = L.tileLayer(template, { ...weatherOptions, opacity });
+  if (weather.layers.length < weather.maxLayers) {
+    const time = weather.time - weather.layers.length * weather.intervalMilliseconds;
+    const visibility = weather.layers.length === 0 ? 'visible' : 'hidden';
+    const layer = L.shingleLayer(getWeatherTemplate(time), { ...weatherOptions, visibility });
     layer.on('load', initWeatherLayers);
-    weather.layers.unshift(layer);
-    layer.addTo(map);
+    weather.layers.unshift(layer.addTo(map));
     return;
   }
 
--- js/leaflet-extensions.js
+++ js/leaflet-extensions.js
@@ -1,33 +1,209 @@
-L.TileLayer.include({
-  getTileUrl: function (coords) {
-    var d = 20037508.342787;
-    var z = this._getZoomForUrl();
-    var w = d / Math.pow(2, z - 1);
-    var minx = -d + coords.x * w;
-    var miny = d - (coords.y + 1) * w;
 
-    var data = {
-      r: L.Browser.retina ? '@2x' : '',
-      s: this._getSubdomain(coords),
-      x: coords.x,
-      y: coords.y,
-      z: z,
-      minx: minx,
-      miny: miny,
-      maxx: minx + w,
-      maxy: miny + w
+L.ShingleLayer = L.Layer.extend({
+  getEvents: function () {
+    var events = {
+      dragend: this._dragEnd,
+      zoom: this._animateZoom
     };
 
-    if (this._map && !this._map.options.crs.infinite) {
-      var invertedY = this._globalTileRange.max.y - coords.y;
-
-      if (this.options.tms) {
-        data['y'] = invertedY;
-      }
-
-      data['-y'] = invertedY;
+    if (this._zoomAnimated) {
+      events.zoomanim = this._animateZoom;
+      events.zoomend = this._zoomEnd;
+      events.moveend = this._moveEnd;
     }
 
+    return events;
+  },
+
+  hide: function () {
+    this._container.style.visibility = 'hidden';
+  },
+
+  initialize: function (url, options) {
+    this._url = url;
+    L.setOptions(this, {
+      pane: 'tilePane',
+      boundsFormat: 'projected',  // 'projected' or 'latlng'
+      preserveOnPan: false,
+      visibility: 'visible',
+      zIndex: 1
+    });
+    L.setOptions(this, options);
+    this._shingles = [];
+    this._isLoading = false;
+  },
+
+  isLoading: function () {
+    return this._isLoading;
+  },
+
+  onAdd: function (map) {
+    if (!this._container) {
+      this._container = L.DomUtil.create('div', 'leaflet-layer', this.getPane());
+
+      if (this.options.hasOwnProperty('zIndex')) {
+        this._container.style.zIndex = this.options.zIndex;
+      }
+
+      this._container.style.visibility = this.options.visibility;
+    }
+
+    this._update(true);
+  },
+
+  onRemove: function (map) {
+    L.DomUtil.remove(this._container);
+    this._container = null;
+  },
+
+  redraw: function () {
+    if (this._map) {
+      this._update(true);
+    }
+
+    return this;
+  },
+
+  show: function () {
+    this._container.style.visibility = 'visible';
+  },
+
+  _animateZoom: function (e) {
+    var map = this._map;
+    var zoom = e.zoom || map.getZoom();
+    var center = e.center || map.getCenter();
+
+    for (var i = 0; i < this._shingles.length; ++i) {
+      var img = this._shingles[i];
+      var scale = Math.pow(2, zoom - img.data.level);
+
+      var nw = img.data.bounds.getNorthWest();
+      var se = img.data.bounds.getSouthEast();
+
+      var position = map._latLngToNewLayerPoint(nw, zoom, center);
+      var size = map._latLngToNewLayerPoint(se, zoom, center)._subtract(position);
+
+      L.DomUtil.setTransform(img, position, scale);
+    }
+  },
+
+  _createImage: function (size) {
+    var img = L.DomUtil.create('img', 'leaflet-shingle' + (this._zoomAnimated ? ' leaflet-zoom-animated' : ''));
+    img.style.position = 'absolute';
+    img.style.width = size.x + 'px';
+    img.style.height = size.y + 'px';
+    img.galleryimg = 'no';
+    img.data = {};
+
+    if (this.options.hasOwnProperty('opacity')) {
+      img.style.opacity = this.options.opacity;
+    }
+
+    img.onselectstart = img.onmousemove = L.Util.falseFn;
+    img.onload = this._onLoad;
+    img.ondragstart = function () { return false; };
+
+    return img;
+  },
+
+  _dragEnd: function () {
+    this._dragged = true;
+  },
+
+  _getShingleUrl: function (size, bbox) {
+    var data = {
+      sx: size.x,
+      sy: size.y,
+      minx: bbox[0],
+      miny: bbox[1],
+      maxx: bbox[2],
+      maxy: bbox[3]
+    };
+
     return L.Util.template(this._url, L.extend(data, this.options));
+  },
+
+  _moveEnd: function () {
+    if (!this._dragged && !this._zoomed) {
+      this._update(true);
+      return;
+    }
+
+    delete this._dragged;
+    delete this._zoomed;
+    var layer = this;
+
+    if (layer._changeHandle) {
+      clearTimeout(layer._changeHandle);
+    }
+
+    layer._changeHandle = setTimeout(function () {
+      delete layer._changeHandle;
+      layer._update();
+    }, 500);
+  },
+
+  _onLoad: function () {
+    var layer = this.data.layer;
+    var zoom = layer._map.getZoom();
+
+    if (this.data.level === zoom) {
+      layer._container.appendChild(this);
+
+      for (var i = layer._shingles.length - 1; i >= 0; --i) {
+        var img = layer._shingles[i];
+
+        if (img.data.level !== zoom || this.data.reset || !layer.options.preserveOnPan) {
+          layer._container.removeChild(img);
+          layer._shingles.splice(i, 1);
+        }
+      }
+
+      delete this.data.reset;
+      layer._shingles.push(this);
+    }
+
+    layer._isLoading = false;
+    layer.fire('load');
+  },
+
+  _update: function (reset) {
+    if (!this._map || !this._url) {
+      return;
+    }
+
+    var map = this._map;
+    var size = map.getSize();
+
+    var img = this._createImage(size);
+    img.data.layer = this;
+    img.data.level = map.getZoom();
+    img.data.reset = reset;
+
+    var position = L.point(0, 0);
+    var nw = map.containerPointToLatLng(position);
+    var se = map.containerPointToLatLng(size);
+    img.data.bounds = L.latLngBounds(L.latLng(se.lat, nw.lng), L.latLng(nw.lat, se.lng));
+    var bbox = [ nw.lng, se.lat, se.lng, nw.lat ];
+
+    if (this.options.boundsFormat === 'projected') {
+      se = map.options.crs.project(se);
+      nw = map.options.crs.project(nw);
+      bbox = [ nw.x, se.y, se.x, nw.y ];
+    }
+
+    position = map.containerPointToLayerPoint(position);
+    L.DomUtil.setPosition(img, position);
+
+    this._isLoading = true;
+    img.src = this._getShingleUrl(size, bbox);
+  },
+
+  _zoomEnd: function () {
+    this._zoomed = true;
   }
 });
+
+L.shingleLayer = function (url, options) {
+  return new L.ShingleLayer(url, options);
+};
